e13b278a6ce79e303dce7e93d70c5a0f582435cd
Kafi Rohman <kafiirohman@gmail.com>
2021-01-19 15:35:27 +0700

    ditambahkan fungsi lain

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Buku;
+use PDF;
 
 class BukuController extends Controller
 {
@@ -19,20 +20,20 @@ class BukuController extends Controller
 	}
 
 	public function push(Request $request)
-    {
-        // set collection
+	{
+		// set collection
 		$buku = new Buku;
 
 		//process insert
 		$buku->judul = $request->post('title');
 		$buku->harga = $request->post('price');
 		$buku->penerbit = $request->post('publisher');
-	
+
 		// save operation
 		$buku->save();
-	
+
 		//redirect to page...
-        return redirect('/home')->with('success', 'Buku berhasil ditambahkan');
+		return redirect('/home')->with('success', 'Buku berhasil ditambahkan');
 	}
 
 	public function edit(Buku $value)
@@ -41,21 +42,29 @@ class BukuController extends Controller
 	}
 
 	public function update(Request $request)
-    {
-        //
+	{
+		//
 		$id = $request->post('id');
-    	$buku = Buku::find($id);
+		$buku = Buku::find($id);
 		$buku->judul = $request->post('title');
 		$buku->harga = $request->post('price');
 		$buku->penerbit = $request->post('publisher');
 
-        $buku->update();
-        return redirect('/home')->with('success', 'Buku berhasil diubah');
-    }
+		$buku->update();
+		return redirect('/home')->with('success', 'Buku berhasil diubah');
+	}
 
-	public function destroy(Request $request){
+	public function destroy(Request $request)
+	{
 		$id = $request->input('id');
 		Buku::find($id)->delete();
 		return redirect('/home')->with('success', 'Buku berhasil dihapus');
-    }
+	}
+
+	public function cetak_pdf()
+	{
+		$buku = Buku::all();
+		$pdf = PDF::loadview('buku_pdf', ['buku' => $buku]);
+		return $pdf->stream();
+	}
 }
